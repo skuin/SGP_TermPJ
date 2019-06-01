475a6d6dc981e1c0390636c8d81f387367bd722c
skuin <skuin2004@gmail.com>
2019-06-01 18:57:03 +0900

    first commit

---
+++ SGP_TermProject/SGP_TermProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SGP_TermProject
+//
+//  Created by kpugame on 2019. 6. 1..
+//  Copyright © 2019년 JEONGUN JO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
